5eb12b5f74119a5a87b91da6f2edeffd28c9959d
Disha Shiyani <dishashiyani2553@gmail.com>
2024-03-30 23:45:40 +0530

    Resume

---
+++ Resume/src/Resume.js
@@ -0,0 +1,235 @@
+import React from 'react';
+
+function Resume(){
+    return(
+        <>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-5'>
+                    <div style={{"backgroundColor":"#006666","float":"left","height":"1500px","width":"400px"}}>
+                    <div style={{"backgroundColor":"#004848","height":"30px","width":"400px"}}> </div>
+                    <div style={{"color":"white","fontFamily":"revert","textAlign":"center","padding":"10px"}}>
+                        <h1>TIMOTHY</h1>
+                        <h1 style={{"marginTop":"-20px"}}>DUNCAN</h1>
+                    </div>
+
+                    <div style={{"paddingLeft":"20px","color":"white"}}>
+                        <h3>SKILLS</h3>
+                    </div>
+                    <div>
+                    <hr class="border border-bs-emphasis-color border-2 opacity-50" style={{"marginLeft":"20px","width":"300px"}}></hr>
+                    </div>
+
+                    <div style={{"color":"white","paddingLeft":"20px"}}>
+                    <p>Communication.Customer-Oriented.</p>
+                    <p>Leadership.Team Coordination.</p>
+                    <p>Negotiation.Business Intelligence.</p>
+                    <p>Critical Thinking.Creativity.</p>
+                    <p>Sales & Marketing.Hubspot.</p>
+                    </div>
+
+                    <div style={{"color":"white","marginTop":"35px","paddingLeft":"20px"}}>
+                        <h3>INDUSTRY EXPERTISE</h3>
+                    </div>
+
+                    <hr class="border border-bs-emphasis-color border-2 opacity-50" style={{"marginLeft":"20px","width":"300px"}}></hr>
+                    <h4 style={{"color":"white","paddingLeft":"20px","marginTop":"2px"}}>
+                        Sales training
+                    </h4>
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"300px","height":"7px","marginLeft":"20px","backgroundColor":"lightgrey"}}>
+                            <div class="progress-bar w-100" style={{"backgroundColor":"white"}}></div>
+                    </div>
+
+                    <h4 style={{"color":"white","paddingLeft":"20px","marginTop":"20px"}}>
+                        Product-led growth
+                    </h4>
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"300px","height":"7px","marginLeft":"20px","backgroundColor":"lightgrey"}}>
+                            <div class="progress-bar w-75" style={{"backgroundColor":"white"}}></div>
+                    </div>
+
+                    <h4 style={{"color":"white","paddingLeft":"20px","marginTop":"20px"}}>
+                        Patnerships
+                    </h4>
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"300px","height":"7px","marginLeft":"20px","backgroundColor":"lightgrey"}}>
+                            <div class="progress-bar w-50" style={{"backgroundColor":"white"}}></div>
+                    </div>
+                    
+                    <div style={{"color":"white","paddingLeft":"20px","marginTop":"35px"}}>
+                        <h3>
+                            Languages
+                        </h3>
+                    </div>
+
+                    <hr class="border border-bs-emphasis-color border-2 opacity-50" style={{"marginLeft":"20px","width":"300px"}}></hr>
+
+                    {/* <div>
+                        <p style={{"color":"white","float":"left"}}>English</p>     
+                    </div>
+
+                    <div>
+                    <p style={{"color":"white","float":"right","marginRight":"100px"}}>Native
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"70px","height":"9px","backgroundColor":"lightgrey","float":"right","marginLeft":"20px","marginTop":"7px"}}>
+                            <div class="progress-bar w-75" style={{"backgroundColor":"white"}}></div>
+                    </div>
+                    </p>            
+                    </div> */}
+                    
+                    <div>
+                        <p style={{"color":"white","float":"left","marginTop":"7px","marginLeft":"20px"}}>English</p>     
+                    </div>
+
+                    <div>
+                    <p style={{"color":"white","float":"right","marginRight":"100px","marginTop":"9px"}}>Native
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"70px","height":"9px","backgroundColor":"lightgrey","float":"right","marginLeft":"20px","marginTop":"9px"}}>
+                            <div class="progress-bar w-75" style={{"backgroundColor":"white"}}></div>
+                    </div>
+                    </p>            
+                    </div>
+
+
+                    <div>
+                        <p style={{"color":"white","float":"left","marginTop":"10px","marginLeft":"20px"}}>Spanish</p>     
+                    </div>
+
+                    <div>
+                    <p style={{"color":"white","float":"right","marginRight":"100px","marginTop":"9px"}}>Advanced
+
+                    <div class="progress progressbar-bs-emphasis-color" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{"width":"70px","height":"9px","backgroundColor":"lightgrey","float":"right","marginLeft":"20px","marginTop":"7px"}}>
+                            <div class="progress-bar w-100" style={{"backgroundColor":"white"}}></div>
+                    </div>
+                    </p>            
+                    </div>
+
+                    <div style={{"color":"white","paddingLeft":"20px","marginTop":"130px"}}>
+                    <h3> FAVOURITE QUOTE </h3>
+                    </div>
+
+                    <hr class="border border-bs-emphasis-color border-2 opacity-50" style={{"marginLeft":"20px","width":"300px"}}></hr>
+
+                <div style={{"color":"white","paddingLeft":"20px"}}>
+                <p>A person who is quietly confident makes the best leader</p>
+
+                <p>----Fred Wilson,Union Ventures</p>
+                </div>
+        
+                <div style={{"color":"white","paddingLeft":"20px","marginTop":"40px"}}>
+                    <h3>PASSIONS</h3>
+                    </div>
+
+                    <hr class="border border-bs-emphasis-color border-2 opacity-50" style={{"marginLeft":"20px","width":"300px"}}></hr>
+
+                    <div style={{"color":"white","paddingLeft":"20px"}}>
+                    <h4>✰ Developing my team</h4>
+                    <p>lorem2 adjkjkfhdailhf,fkhlkhfdh</p>
+                    </div>
+
+                    <div style={{"color":"white","paddingLeft":"20px"}}>
+                    <h4>🤍 Giving back to community</h4>
+                    <p>lorem2 adjkjkfhdailhf,fkhlkhfdh</p>
+                    </div>
+
+                    <div style={{"color":"white","paddingLeft":"20px"}}>
+                    <h4> 🦄 Horse-riding</h4>
+                    <p>lorem2 adjkjkfhdailhf,fkhlkhfdh</p>
+                    </div>
+
+                    <div style={{"paddingLeft":"20px"}}>
+                        <p style={{"float":"left","color":"white","paddingRight":"80px","fontSize":"15px","marginTop":"20px"}}>Powered by</p>
+
+                        <div style={{"float":"right","color":"white","paddingRight":"80px","fontSize":"15px","marginTop":"20px"}}>♾ Enhancy</div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                 <div className='col-7' style={{"marginTop":"-1460px","marginLeft":"450px","float":"right"}}>
+                 {/* <h1 style={{"fontSize":"30px","color":"#24435c"}}>JAMES MOORE</h1> */}
+                        <h3 style={{"fontSize":"22px","color":"#3abbbd","marginTop":"-12px"}}>Director of Business Devlopment</h3>
+                        <p style={{"fontSize":"13px","marginTop":"-5px"}}>&#128222; (862) 7345-425   @timothy.duncan@gmail.com</p>
+                        <p style={{"fontSize":"13px","marginTop":"-15px"}}>&#128279; linkedin.com/in/timothy-duncan-jr    &#x1F4CD; Paterson ,New Jersey</p>
+                
+                        <h3 style={{"marginTop":"35px"}}>SUMMARY</h3>
+                        <hr style={{"marginTop":"5px"}}></hr>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                    <div style={{"color":"black","marginTop":"30px"}}>
+                        <h3>KEY ACHIEVEMENTS</h3>
+                        <hr style={{"marginTop":"-5px"}}></hr>
+                        <div>
+                            <h5>📈 Sales Training Expert</h5>
+                            <p>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived typesetting, remaining essentially unchanged.</p>
+
+                            <h5>🤝🏻 Negotiation</h5>
+                            <p>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived typesetting, remaining essentially unchanged.</p>
+
+                            <h5>💎 Leadership in Times of Crisis</h5>
+                            <p>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived typesetting, remaining essentially unchanged.</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 style={{"marginTop":"25px"}}>EXPERIENCE</h3>
+                        <hr style={{"marginTop":"-5px"}}></hr>
+
+                        <div>
+                            <h3 style={{"color":"#24435c","fontSize":"25px","marginTop":"-8px"}}>Director of Business Development</h3>
+                            <p style={{"float":"right","marginTop":"-30px","marginRight":"20px"}}>2016 - Ongoing</p>
+                            <h4 style={{"fontSize":"22px","color":"#3abbbd","marginTop":"-5px"}}>Esycron</h4>
+                            <p style={{"float":"right","marginTop":"-30px","marginRight":"20px"}}>New Jersey</p>
+                            <p style={{"marginTop":"10px"}}>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            <ul style={{"marginTop":"-13px"}}>
+                                <li>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </li>
+                                <li>1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </li>
+                                <li>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.  </li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3 style={{"color":"#24435c","fontSize":"25px"}}>Sales Representative</h3>
+                            <p style={{"float":"right","marginTop":"-30px","marginRight":"20px"}}>2011 - 2016</p>
+                            <h4 style={{"fontSize":"22px","color":"#3abbbd","marginTop":"-2px"}}>ReloUnion</h4>
+                            <p style={{"float":"right","marginTop":"-35px","marginRight":"20px"}}>New Jersey</p>
+                            <p style={{"marginTop":"-5px"}}>Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.</p>
+                            <ul style={{"marginTop":"-15px"}}>
+                                <li>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </li>
+                                <li>1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 style={{"marginTop":"30px","fontSize":"25px"}}>EDUCATION</h3>
+                        <hr style={{"marginTop":"5px"}}></hr>
+
+                        <p style={{"fontSize":"20px","color":"#24435c"}}>MSc in Marketing Science</p>
+                        <p style={{"float":"right","marginTop":"-40px","marginRight":"20px"}}>2006 - 2007</p>
+                        <p style={{"fontSize":"20px","color":"#3abbbd","marginTop":"-15px","fontWeight":"bold"}}>Columbus University</p>
+                        <p style={{"fontSize":"20px","color":"#24435c"}}>BSc in Marketing</p>
+                        <p style={{"float":"right","marginTop":"-40px","marginRight":"20px"}}>2002 - 2005</p>
+                        <p style={{"fontSize":"20px","color":"#3abbbd","marginTop":"-15px","fontWeight":"bold"}}>Rutgers University</p>
+                    </div>
+
+                    <div>
+                        
+                        <p style={{"float":"right","marginRight":"20px","marginBottom":"-20px","fontSize":"13px"}}>www.enhancv.com</p>
+                    </div>
+                </div>
+
+                 </div>
+            </div>
+
+   
+      
+        </>
+    );
+}
+
+
+
+
+export default Resume;
